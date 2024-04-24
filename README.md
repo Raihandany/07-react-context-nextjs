8b16ac976d05428f693c5abb369b7115ae26a2cc
Raihandany <raihandaniy@gmail.com>
2024-04-25 00:23:06 +0700

    W07 : Practicum - 1 soal 2

--- README.md
+++ README.md
@@ -8,8 +8,22 @@
 | Kelas | TI - 3I |
 
 
-## Practicum - 1
+## Practicum - 1 a
 Capture hasilnya dan buatlah laporan di README.md. Jelaskan apa yang telah Anda pelajari dan bagaimana tampilannya saat ini?
 Answer : 
     In the results of this website in practicum 01, it displays the title and sections on the main page of the website. This can be done using components from the components folder. In the component file folder there are files such as atoms (which store headings and sections) and templates (store main_page files) these are called child components which will later be needed/appeared in the parent component (main_pages).
+Result Practicum :
 |![Screenshot](assets-report/07-jawaban-soal-1.PNG)
+
+## Practicum - 1 b
+Capture hasilnya dan buatlah laporan di README.md. Jelaskan apa yang telah Anda pelajari dan bagaimana tampilannya saat ini?
+
+Jika terjadi error, silakan perbaiki, Mengapa hal itu bisa terjadi? Jelaskan!
+
+Answer : 
+    From the practice result the Error shows that I get, I think this is happening because the createContext function only works in the Client component instead of the Server component which is normally created in Next.js. To solve this problem I found we need to use client code in each related file (mycontext.tsx, heading.tsx, section.tsx).
+Result Practicum :
+Default Error Code
+|![Screenshot](assets-report/07-jawaban-soal-1b.PNG)
+Solve Code
+|![Screenshot](assets-report/07-jawaban-soal-12b.PNG)
--- src/components/templates/main_page.tsx
+++ src/components/templates/main_page.tsx
@@ -3,21 +3,21 @@ import Section from "../atoms/section";
 
 export default function MainPage() {
     return (
-        <Section>
-            <Heading level={1}>Title</Heading>
-            <Section>
-                <Heading level={2}>Heading</Heading>
-                <Heading level={2}>Heading</Heading>
-                <Heading level={2}>Heading</Heading>
-                <Section>
-                    <Heading level={3}>Sub-heading</Heading>
-                    <Heading level={3}>Sub-heading</Heading>
-                    <Heading level={3}>Sub-heading</Heading>
-                    <Section>
-                        <Heading level={4}>Sub-sub-heading</Heading>
-                        <Heading level={4}>Sub-sub-heading</Heading>
-                        <Heading level={4}>Sub-sub-heading</Heading>
-                    </Section>
+        <Section level = {1}>
+            <Heading>Judul</Heading>
+            <Section level={2}>
+                <Heading>Heading</Heading>
+                <Heading>Heading</Heading>
+                <Heading>Heading</Heading>
+                <Section level={3}>
+                    <Heading>Sub-heading</Heading>
+                    <Heading>Sub-heading</Heading>
+                    <Heading>Sub-heading</Heading>
+                </Section>
+                <Section level={4}>
+                    <Heading>Sub-sub-heading</Heading>
+                    <Heading>Sub-sub-heading</Heading>
+                    <Heading>Sub-sub-heading</Heading>
                 </Section>
             </Section>
         </Section>
